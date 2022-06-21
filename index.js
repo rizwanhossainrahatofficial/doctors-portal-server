37034fe27e82e98fea6ac4c039a51e6d2f01582d
rizwanhossainrahat <rizwanhossainrahat400@gmail.com>
2022-06-21 21:16:13 +0600

    server ready

---
+++ index.js
@@ -0,0 +1,142 @@
+const express = require('express')
+const app = express()
+var admin = require("firebase-admin");
+const cors=require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const port =process.env.PORT|| 5000;
+
+
+
+var serviceAccount = require('./doctors-portal-firebase-adminsdk.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+app.use(cors())
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.iwcqk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function verifyToken (req,res,next){
+  if(req.headers?.authorization?.startsWith('Bearer ')){
+    const token=req.headers.authorization.split(' ')[1];
+
+    try{
+      const decodedUser=await admin.auth().verifyIdToken(token);
+      req.decodedEmail=decodedUser.email
+    }
+    catch{
+
+    }
+  }
+  next();
+}
+
+
+async function run(){
+  try{
+      await client.connect();
+      const database = client.db("doctors-portal");
+      const appointmentsCollection=database.collection('appointments')
+      const usersCollection=database.collection('users')
+
+// send data server site to client site
+      app.get('/appointments', async(req,res)=>{
+        const email=req.query.email;
+        const date=new Date(req.query.date).toLocaleDateString();
+        console.log(req.query.date)
+        const query={email:email,date:date};     
+       
+        const cursor=appointmentsCollection.find(query);
+        const appointments=await cursor.toArray();
+        res.json(appointments);
+      })
+
+      // post use to send data from client site to server site
+      app.post('/appointments',verifyToken,async(req,res)=>{
+          const appointment=req.body;
+          const result=await appointmentsCollection.insertOne(appointment)
+          res.json(result)
+      })
+
+
+      app.get('/users/:email',async(req,res)=>{
+        const email=req.params.email;
+        const query={email:email}
+        const user=await usersCollection.findOne(query)
+        let isAdmin=false;
+        if(user?.role === 'admin'){
+          isAdmin=true;
+        }
+        res.json({admin:isAdmin});
+      })
+
+      // send register data to the database
+      app.post('/users',async(req,res)=>{
+        const user=req.body;
+        const result=await usersCollection.insertOne(user);
+        console.log(result);
+        res.json(result)
+      })
+      
+      
+        app.put('/users',async(req,res)=>{
+          const user=req.body;
+          const filter={email: user.email}
+          const option = { upsert: true };
+          const updateDoc={$set:user};
+          const result= await usersCollection.updateOne(filter,updateDoc,option)
+          res.json(result)
+        })
+
+      // make admin
+        app.put('/users/admin', verifyToken,async(req,res)=>{
+          const user=req.body
+          const requester= req.decodedEmail
+          if(requester){
+            const requesterAccount=await usersCollection.findOne({email:requester});
+            if(requesterAccount.role==='admin'){
+
+              const filter={email:user.email}
+          const updateDoc={$set:{role:'admin'}}
+          const result=await usersCollection.updateOne(filter,updateDoc);
+          res.json(result); 
+            }
+          }
+              else{
+                res.status(403).json({message:'you do not have allow to make admin'})
+              }     
+        })
+
+  }
+  finally{
+      // await client.close();
+  }
+
+}
+
+run().catch(console.dir)
+
+app.get('/', (req, res) => {
+  res.send('Hello doctors portals!')
+})
+
+app.listen(port, () => {
+  console.log(` listening on port ${port}`)
+})
+
+
+
+
+// app.get('/users')
+// app.post('/users')
+// app.get('/users/:id') 
+// app.put('/users/:id') user k update kore
+// app.delete('/users/:id') user k delete kore
+// upset google sign in ar somoy bebohar kora hoi jodi user thake tahole add korbe na. r jodi na thake tahole add korbe
